449fa85a6281433aa727108b32efef32c9656340
glorino <glorinomart@gmail.com>
2026-08-11 10:51:10 +0100

    Update contact phone and address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,16 +4,16 @@ export const SITE = {
 };
 
 export const CONTACT = {
-  phone: "+234 (0) 800 MANN PRO",
-  phoneRaw: "+2348000000000",
+  phone: "+234 803 636 4215",
+  phoneRaw: "+2348036364215",
   phone2: "+234 (0) 801 234 5678",
   phone2Raw: "+2348012345678",
   email: "contact@mannprofessionalservices.com",
   emailInfo: "info@mannprofessionalservices.com",
   emailAudit: "audit@mannprofessionalservices.com",
   website: "www.mannprofessionalservices.com",
-  address: "Lagos, Nigeria",
-  address2: "Abuja, Nigeria",
+  address: "3, Sadatu Street, Oregun",
+  address2: "Lagos State, Nigeria",
 };
 
 export const SOCIALS = {
